709232307510c1e0d38767bfc60ab170f1f80c9f
Tejas Bhavsar <tbhavsar121@gmail.com>
2021-04-30 13:12:06 +0530

    Added blockly to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import './customBlocks/custom_Blocks'
+import React from 'react'
+import ReactBlockly from 'react-blockly'
+import Blockly from 'blockly';
 
-function App() {
+export default function App() {
+  const initialXml = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
+  const toolboxCategories = [
+    {
+      name: 'Logic',
+      colour: '#5C81A6',
+      blocks: [
+        {
+          type: 'controls_if'
+        },
+        {
+          type: 'logic_compare'
+        }
+      ]
+    },
+    {
+      name: 'Math',
+      colour: '#5CA65C',
+      blocks: [
+        {
+          type: 'math_round'
+        },
+        {
+          type: 'math_number'
+        }
+      ]
+    },
+    {
+      name: 'Custom',
+      colour: '#5CA699',
+      blocks: [
+        {
+          type: 'new_boundary_function'
+        },
+        {
+          type: 'return'
+        },
+      ]
+    }
+  ]
+  function workspaceDidChange(workspace) {
+    const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+    document.getElementById('generated-xml').innerText = newXml;
+
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+    document.getElementById('code').value = code;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ReactBlockly
+        toolboxCategories={toolboxCategories}
+        wrapperDivClassName="fill-height"
+        workspaceConfiguration={{
+          grid: {
+            spacing: 20,
+            length: 3,
+            colour: '#ccc',
+            snap: true,
+          },
+        }}
+        workspaceDidChange={workspaceDidChange}
+      />
+      <pre id="generated-xml">
+      </pre>
+      <textarea id="code" style={{ height: "200px", width: "400px" }} value=""></textarea>
+    </>
+  )
 }
-
-export default App;
