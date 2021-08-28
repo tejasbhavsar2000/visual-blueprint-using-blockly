5fcb2e42b5896439e9391630260cbac9f71fba81
unknown <tbhavsar121@gmail.com>
2021-08-28 20:03:42 +0530

    added new Blocks

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -19,13 +19,31 @@ export default function Blocks() {
           type: "logic_compare",
         },
         {
+          type: "logic_boolean",
+        },
+        {
           type: "logic_operation",
+        },
+        {
+          type: "controls_ifelse",
+        },
+        {
+          type: "logic_compare",
+        },
+        {
+          type: "logic_negate",
+        },
+        {
+          type: "logic_null",
+        },
+        {
+          type: "logic_ternary",
         },
       ],
     },
     {
       name: "Loops",
-      colour: "#5C81A6",
+      colour: "rgb(91, 165, 91)",
       blocks: [
         {
           type: "controls_repeat",
@@ -36,11 +54,20 @@ export default function Blocks() {
         {
           type: "controls_whileUntil",
         },
+        {
+          type: "controls_for",
+        },
+        {
+          type: "controls_forEach",
+        },
+        {
+          type: "controls_flow_statements",
+        },
       ],
     },
     {
       name: "Math",
-      colour: "#5CA65C",
+      colour: "rgb(91, 103, 165)",
       blocks: [
         {
           type: "math_round",
@@ -50,6 +77,123 @@ export default function Blocks() {
         },
         {
           type: "math_arithmetic",
+        },
+        {
+          type: "math_single",
+        },
+        {
+          type: "math_trig",
+        },
+        {
+          type: "math_constant",
+        },
+        {
+          type: "math_number_property",
+        },
+        {
+          type: "math_change",
+        },
+        {
+          type: "math_on_list",
+        },
+        {
+          type: "math_modulo",
+        },
+        {
+          type: "math_constrain",
+        },
+        {
+          type: "math_random_int",
+        },
+        {
+          type: "math_random_float",
+        },
+      ],
+    },
+    {
+      name: "Lists",
+      colour: "rgb(116, 91, 165)",
+      blocks: [
+        {
+          type: "lists_create_empty",
+        },
+        {
+          type: "lists_repeat",
+        },
+        {
+          type: "lists_reverse",
+        },
+        {
+          type: "lists_isEmpty",
+        },
+        {
+          type: "lists_length",
+        },
+      ],
+    },
+    {
+      name: "Text",
+      colour: "rgb(91, 165, 140)",
+      blocks: [
+        {
+          type: "text",
+        },
+        {
+          type: "text_multiline",
+        },
+        {
+          type: "text_join",
+        },
+        {
+          type: "text_create_join_container",
+        },
+        {
+          type: "text_create_join_item",
+        },
+        {
+          type: "text_append",
+        },
+        {
+          type: "text_length",
+        },
+        {
+          type: "text_isEmpty",
+        },
+        {
+          type: "text_indexOf",
+        },
+        {
+          type: "text_charAt",
+        },
+      ],
+    },
+    {
+      name: "Variables",
+      colour: "rgb(165, 91, 128)",
+      blocks: [
+        {
+          type: "variables_get",
+        },
+        {
+          type: "variables_set",
+        },
+      ],
+    },
+    {
+      name: "Procedures",
+      colour: "rgb(153, 91, 165)",
+      blocks: [
+        {
+          type: "procedures_defreturn",
+        },
+        {
+          type: "procedures_defnoreturn",
+        },
+        {
+          type: "procedures_callreturn",
+        },
+        {
+          type: "procedures_callnoreturn",
         },
       ],
     },
